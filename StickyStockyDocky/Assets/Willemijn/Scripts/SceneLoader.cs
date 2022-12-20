dbb08564ef338d6affbc35e36d71928a743b037e
Job <0326531@student.rocvantwente.nl>
2022-12-20 12:35:22 +0100

    errors fixed

--- StickyStockyDocky/Assets/Willemijn/Scripts/SceneLoader.cs
+++ StickyStockyDocky/Assets/Willemijn/Scripts/SceneLoader.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    public GameObject soundeffects;
-    public GameObject music;
+    public Toggle soundeffects;
+    public Toggle music;
+    /// <summary>
+    /// Loads the scene at the given BuildIndex. if <paramref name="scene"/> is set to 10 the application will quit
+    /// </summary>
+    /// <param name="scene"></param>
     public void LoadScene (int scene)
     {
         if (scene == 10) { Application.Quit(); }
@@ -14,12 +19,12 @@ public class SceneLoader : MonoBehaviour
     }
     public void SetMusicValue()
     {
-        Debug.Log(music.GetComponent<Toggle>().value);
-        PlayerPrefs.SetInt("PlayMusic", music.GetComponent<Toggle>().value ? 1 : 0);
+        Debug.Log(music.isOn);
+        PlayerPrefs.SetInt("PlayMusic", music.isOn ? 1 : 0);
     }
     public void SetSoundEffectValue()
     {
-        Debug.Log(soundeffects.GetComponent<Toggle>().value);
-        PlayerPrefs.SetInt("PlayEffects", soundeffects.GetComponent<Toggle>().value ? 1 : 0);
+        Debug.Log(soundeffects.isOn);
+        PlayerPrefs.SetInt("PlayEffects", soundeffects.isOn ? 1 : 0);
     }
 }
