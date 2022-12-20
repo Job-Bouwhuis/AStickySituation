623803746bb957c4af47f87a8d072e9da7149a99
Job <0326531@student.rocvantwente.nl>
2022-12-20 11:27:43 +0100

    music added.

---
+++ StickyStockyDocky/Assets/Scripts/BackgroundMusicPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicPlayer : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.loop = true;
+        source.clip = music;
+        source.playOnAwake = false;
+         
+        bool shouldPlay = PlayerPrefs.GetInt("PlayMusic") == 1;
+        source.Play();
+    }
+}
