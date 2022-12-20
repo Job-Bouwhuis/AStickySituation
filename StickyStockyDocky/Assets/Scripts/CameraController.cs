8cb72d313378995e58f8cc6adfaab0ae4df5b2c8
Dion Vos <dionvos160@gmail.com>
2022-12-20 13:07:26 +0100

    First Working Product

--- StickyStockyDocky/Assets/Scripts/CameraController.cs
+++ StickyStockyDocky/Assets/Scripts/CameraController.cs
@@ -7,15 +7,11 @@ public class CameraController : MonoBehaviour
     [H("Settings")]
     [SF] private Transform target;
     [SF] private float followSpeed = 5000f;
-    [SF] private Transform background;
 
     // Update is called once per frame
     void Update()
     { 
         if(Time.timeSinceLevelLoad > 1)
-        {
             transform.position = Vector3.Lerp(transform.position, new(target.position.x, target.position.y, -10), Time.deltaTime * followSpeed);
-            background.transform.position = Vector3.Lerp(background.transform.position, new(target.position.x, target.position.y, 0), Time.deltaTime * followSpeed);
-        }
     }
 }
